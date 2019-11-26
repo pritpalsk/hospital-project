5333ceccfdcc52084c73ff6b84425e3b7c7183b0
pritpalsk <psinghspt@gmail.com>
2019-11-26 11:40:05 -0600

    setup laravel

---
+++ app/Patient.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Patient extends Model
+{
+    public $attributes = [];
+    public $fillable = [];
+    public $hidden = [];
+}
